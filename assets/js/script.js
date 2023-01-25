cde0f7eeb9acb1f69086cc861f7516aa67be1490
vivienrauch <vivienmirjam@gmail.com>
2023-01-25 14:10:31 +0000

    Add transition from welcome page to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,41 +11,9 @@
 
 
 /* Load page and add listeners for clicking the start and next buttons or pressing Enter. */
-
-document.addEventListener("DOMContentLoaded", function() {
-
-    rules.classList.remove('hide');
-   
-    let startButton = document.getElementsByClassName('start-game');
-
-    for (let button of startButton) {
-        button.addEventListener('click', function() {
-                helloPlayer();
-                startGame();
-        })
-    }
-
-    document.getElementById('username').addEventListener('keydown', function(event) {
-        if (event.key === 'Enter') {
-            helloPlayer();
-            startGame();
-        }
-    })
-
-
-    let nextButton = document.getElementById('next-question');
-
-    for (let button of nextButton) {
-        button.addEventListener('click', function () {
-                nextQuestion();
-        })
-    }
-
-})
-
 /* Adding variables */
 
-const questions = [
+/*const questions = [
     {
         question : 'What is the capital of Malawi?',
         choiceA : 'Nairobi',
@@ -182,7 +150,6 @@ const questions = [
     },
 ];
 
-
 let question = document.addElementById('question');
 let choiceA = document.getElementById('choiceA');
 let choiceB = document.getElementById('choiceB');
@@ -192,50 +159,386 @@ let scores = document.getElementById('scores');
 let quiz = document.getElementById('game-frame');
 let username = document.getElementById('username');
 let rules = document.getElementById('rules-panel');
-let finalScores = document.getElementById('score-panel');
-
-
+let results = document.getElementById('results-panel');*/
+    
+/*rules.style.visibility = 'visible';
 
 let lastQuestion = questions.length-1;
-let runningQ = 0;
+let runningQ = 0;*/
+
+ document.addEventListener('DOMContentLoaded', function() {
+    let startButton = document.getElementsByClassName('start-game');
+    let rules = document.getElementById('rules-panel');
+    let results = document.getElementById('results-panel');
+
+    rules.classList.remove('hide');
+    results.style.display = 'none';
+    
+
+    for (let button of startButton) {
+        button.addEventListener('click', function() {
+                helloPlayer();
+        })
+    }
+
+    document.getElementById('username').addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            helloPlayer();
+        }
+    })
+
+
+    let nextButton = document.getElementById('next-question');
+
+    for (let button of nextButton) {
+        button.addEventListener('click', function () {
+                nextQuestion();
+        })
+    }
+
 
 
 /* Renders questions */
 
-function renderQuestion() {
+/*function renderQuestion() {
+
+    rules.style.display = 'none';
+    let quiz = document.getElementById('game-frame');
+    quiz.classList.remove('hide');
+
+    const questions = [
+        {
+            question : 'What is the capital of Malawi?',
+            choiceA : 'Nairobi',
+            choiceB : 'Maputo',
+            choiceC : 'Lilongwe',
+            choiceD : 'Lusaka',
+            correctAnswer: 'd'
+        },
+    
+        {
+            question : 'Manila is the capital of...',
+            choiceA : 'Philippines',
+            choiceB : 'Afghanistan',
+            choiceC : 'Andorra',
+            choiceD : 'Mozambique',
+            correctAnswer: 'a'
+        },
+    
+        {
+            question : 'Vientiane is the capital of...',
+            choiceA : 'Thailand',
+            chocieB : 'Vietnam',
+            choiceC : 'Laos',
+            choiceD : 'Taiwan',
+            correctAnswer : 'c'
+        },
+    
+        {
+            question : 'What is the capital of Albania?',
+            chocieA : 'Bishkek',
+            choiceB : 'Sarajevo',
+            choiceC : 'Minsk',
+            choiceD : 'Tirana',
+            correctAnswer: 'd'
+        },
+    
+        {
+            question : 'What is the capital of Chile?',
+            chocieA : 'Lima',
+            choiceB : 'Santiago',
+            choiceC : 'Caracas',
+            choiceD : 'Quito',
+            correctAnswer : 'b'
+        },
+    
+        {
+            question : 'Montevideo is the capital of...',
+            choiceA : 'Cuba',
+            choiceB : 'Hawaii',
+            choiceC : 'Uruguay',
+            choiceD : 'Paraguay',
+            correctAnswer : 'c'
+        },
+    
+        {
+            question : 'What is the capital of Colombia?',
+            choiceA : 'Buenos Aires',
+            choiceB : 'Paramaribo',
+            choiceC : 'Bogotá',
+            choiceD : 'La Paz',
+            correctAnswer: 'c'
+        },
+    
+        {
+            question : 'Budapest is the capital of...',
+            choiceA : 'Romania',
+            choiceB : 'Iceland',
+            choiceC : 'Slovakia',
+            choiceD : 'Hungary',
+            correctAnswer : 'd'
+        },
+    
+        {
+            question : 'What is the capital of Latvia?',
+            choiceA : 'Tallinn',
+            choiceB : 'Riga',
+            choiceC : 'Vilnius',
+            choiceD : 'Oslo',
+            correctAnswer : 'b'
+        },
+    
+        {
+            question : 'What is the capital of Belgium?',
+            choiceA : 'Brussels',
+            choiceB : 'Bern',
+            choiceC : 'Berlin',
+            choiceD : 'Bonn',
+            correctAnswer : 'a'
+        },
+    
+        {
+            question : 'Jerusalem is the capital of...',
+            choiceA : 'Egypt',
+            choiceB : 'Jordan',
+            choiceC : 'Israel',
+            choiceD : 'Lebanon',
+            correctAnswer : 'c'
+        },
+    
+        {
+            question : 'What is the capital of Sweden?',
+            choiceA : 'Malmö',
+            choiceB : 'Göteborg',
+            choiceC : 'Uppsala',
+            choiceD : 'Stockholm',
+            correctAnswer : 'd'
+        },
+    
+        {
+            question : 'What is the capital of New Zealand?',
+            choiceA : 'Canberra',
+            choiceB : 'Wellington',
+            choiceC : 'Ottawa',
+            choiceD : 'Brisbane',
+            correctAnswer : 'b'
+        },
+    
+        {
+            question : 'Canberra is the capital of...',
+            choiceA : 'Monaco',
+            choiceB : 'Lichtenstein',
+            choiceC : 'Australia',
+            choiceD : 'Croatia',
+            correctAnswer : 'c'
+        },
+    
+        {
+            question : 'Yaoundé is the capital of...',
+            choiceA : 'Mali',
+            choiceB : 'Ghana',
+            choiceC : 'Congo',
+            choiceD : 'Cameroon',
+            correctAnswer : 'd'
+        },
+    ];
+    
+    let question = document.addElementById('question');
+    let choiceA = document.getElementById('choiceA');
+    let choiceB = document.getElementById('choiceB');
+    let choiceC = document.getElementById('choiceC');
+    let choiceD = document.getElementById('choiceD');
+        
+    let runningQ = 0;
     let q = questions[runningQ];
 
-    question.innerHTML = '<p>' + q.question[0][0] + '</p>';
-    choiceA.innerHTML = q.choiceA[0][1];
-    choiceB.innerHTML = q.choiceB[0][2];
-    choiceC.innerHTML = q.choiceC[0][3];
-    choiceD.innerHTML = q.choiceD[0][4];
-};
+    for (i = 0; i <= questions.length; i++) {
+    question.innerHTML = '<p>' + q.question[i] + '</p>';
+    choiceA.innerHTML = q.choiceA[i];
+    choiceB.innerHTML = q.choiceB[i];
+    choiceC.innerHTML = q.choiceC[i];
+    choiceD.innerHTML = q.choiceD[i];
+    }
+};*/
 
-
-function startGame() {
-    renderQuestion();
-}
-
-/** Greets the player and starts the game when they enter a usernamename.
- * Gives an alert if no name is entered and not going to proceed to the game.*/
 
 function helloPlayer () {
 
     if (username.value == "") {
         alert('Please enter your name to start the game!');
         return false;
+
     } else {
         let userLogin = document.getElementById('user-login');
         userLogin.style.display= 'none';
+        rules.style.display = 'none';
+        let quiz = document.getElementById('game-frame');
+        quiz.classList.remove('hide');
+
         let welcomeMessage = document.getElementById('hidden-welcome');
         welcomeMessage.innerHTML = `Welcome ${username.value} ! Let's play!`;
         startGame();
 
     }
 }
+ }) 
 
-userLogin.style.display = 'none';
+
+function startGame() {
+
+    const questions = [
+        {
+            question : 'What is the capital of Malawi?',
+            choiceA : 'Nairobi',
+            choiceB : 'Maputo',
+            choiceC : 'Lilongwe',
+            choiceD : 'Lusaka',
+            correctAnswer: 'd'
+        },
+    
+        {
+            question : 'Manila is the capital of...',
+            choiceA : 'Philippines',
+            choiceB : 'Afghanistan',
+            choiceC : 'Andorra',
+            choiceD : 'Mozambique',
+            correctAnswer: 'a'
+        },
+    
+        {
+            question : 'Vientiane is the capital of...',
+            choiceA : 'Thailand',
+            chocieB : 'Vietnam',
+            choiceC : 'Laos',
+            choiceD : 'Taiwan',
+            correctAnswer : 'c'
+        },
+    
+        {
+            question : 'What is the capital of Albania?',
+            chocieA : 'Bishkek',
+            choiceB : 'Sarajevo',
+            choiceC : 'Minsk',
+            choiceD : 'Tirana',
+            correctAnswer: 'd'
+        },
+    
+        {
+            question : 'What is the capital of Chile?',
+            chocieA : 'Lima',
+            choiceB : 'Santiago',
+            choiceC : 'Caracas',
+            choiceD : 'Quito',
+            correctAnswer : 'b'
+        },
+    
+        {
+            question : 'Montevideo is the capital of...',
+            choiceA : 'Cuba',
+            choiceB : 'Hawaii',
+            choiceC : 'Uruguay',
+            choiceD : 'Paraguay',
+            correctAnswer : 'c'
+        },
+    
+        {
+            question : 'What is the capital of Colombia?',
+            choiceA : 'Buenos Aires',
+            choiceB : 'Paramaribo',
+            choiceC : 'Bogotá',
+            choiceD : 'La Paz',
+            correctAnswer: 'c'
+        },
+    
+        {
+            question : 'Budapest is the capital of...',
+            choiceA : 'Romania',
+            choiceB : 'Iceland',
+            choiceC : 'Slovakia',
+            choiceD : 'Hungary',
+            correctAnswer : 'd'
+        },
+    
+        {
+            question : 'What is the capital of Latvia?',
+            choiceA : 'Tallinn',
+            choiceB : 'Riga',
+            choiceC : 'Vilnius',
+            choiceD : 'Oslo',
+            correctAnswer : 'b'
+        },
+    
+        {
+            question : 'What is the capital of Belgium?',
+            choiceA : 'Brussels',
+            choiceB : 'Bern',
+            choiceC : 'Berlin',
+            choiceD : 'Bonn',
+            correctAnswer : 'a'
+        },
+    
+        {
+            question : 'Jerusalem is the capital of...',
+            choiceA : 'Egypt',
+            choiceB : 'Jordan',
+            choiceC : 'Israel',
+            choiceD : 'Lebanon',
+            correctAnswer : 'c'
+        },
+    
+        {
+            question : 'What is the capital of Sweden?',
+            choiceA : 'Malmö',
+            choiceB : 'Göteborg',
+            choiceC : 'Uppsala',
+            choiceD : 'Stockholm',
+            correctAnswer : 'd'
+        },
+    
+        {
+            question : 'What is the capital of New Zealand?',
+            choiceA : 'Canberra',
+            choiceB : 'Wellington',
+            choiceC : 'Ottawa',
+            choiceD : 'Brisbane',
+            correctAnswer : 'b'
+        },
+    
+        {
+            question : 'Canberra is the capital of...',
+            choiceA : 'Monaco',
+            choiceB : 'Lichtenstein',
+            choiceC : 'Australia',
+            choiceD : 'Croatia',
+            correctAnswer : 'c'
+        },
+    
+        {
+            question : 'Yaoundé is the capital of...',
+            choiceA : 'Mali',
+            choiceB : 'Ghana',
+            choiceC : 'Congo',
+            choiceD : 'Cameroon',
+            correctAnswer : 'd'
+        },
+    ];
+    
+    let question = document.addElementById('question');
+    let choiceA = document.getElementById('choiceA');
+    let choiceB = document.getElementById('choiceB');
+    let choiceC = document.getElementById('choiceC');
+    let choiceD = document.getElementById('choiceD');
+
+    let runningQ = 0;
+    let q = questions[runningQ];
+
+    question.innerHTML = questions[runningQ].;
+}
+
+/** Greets the player and starts the game when they enter a usernamename.
+ * Gives an alert if no name is entered and not going to proceed to the game.*/
+
+
 
 function nextQuestion() {
     if (runningQ <= lastQuestion) {
